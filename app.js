2e30bf3beefe36cd673d6d2c549acbff4b2fc4ee
lorenacardenas06 <lorenacardenas06@gmail.com>
2022-09-05 22:27:07 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express'); //Traigo la libreria express
+
+const path = require('path'); //Traigo la ruta
+
+const app = express(); //Utilizo express
+
+app.use(express.static(path.join(__dirname, './public'))); // Se hace la carpeta public publica
+
+app.get('/shoppingCar', (req,res) =>{
+    res.sendFile(path.join(__dirname, './views/shoppingCar.html'));  // Permite enviar un archivo HTML
+});
+
+app.get('*', (req,res) =>{
+    res.send("Ruta restringida, hemos localizado la direccion de su computador");  // Permite enviar texto o codigo HTML
+});
+
+/*
+app.listen(3002, () => {
+    console.log("Servidor corriendo");
+});
+
+*/
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Servidor corriendo");
+})
