1332355ea86fc5c837dd817ef8037020965d6f89
lorenacardenas06 <lorenacardenas06@gmail.com>
2022-09-27 19:05:00 -0500

    Cambios 27/09/22

--- src/controllers/productosControllers.js
+++ src/controllers/productosControllers.js
@@ -1,8 +1,5 @@
 const controladorProductos = {
   index: (req, res) => {
-    let dir = __dirname;
-    console.log(dir);
-    //res.sendFile(__dirname,"../views/home.html");
     res.render("home");
   }, //mostrar listado de productos
   show: (req, res) => {
