39ec84f1b14eff6b2cf6574382a44c22c7291234
lorenacardenas06 <lorenacardenas06@gmail.com>
2022-09-30 06:50:46 -0500

    Lorena cambios

--- app.js
+++ app.js
@@ -17,10 +17,6 @@ app.listen(process.env.PORT || 3000, function () {
 });
 
 /*
-app.get('/home', (req,res) =>{
-  res.sendFile(path.join(__dirname, './views/home.html'));  // Permite enviar un archivo HTML
-});
-
 app.get('/login', (req,res) =>{
      res.sendFile(path.join(__dirname, './views/login.html'));  // Permite enviar un archivo HTML
  });
@@ -29,15 +25,18 @@ app.get('/productDetail', (req,res) =>{
      res.sendFile(path.join(__dirname, './views/productDetail.html'));  // Permite enviar un archivo HTML
  });
 
-app.get('/shoppingCar', (req,res) =>{
-    res.sendFile(path.join(__dirname, './views/shoppingCar.html'));  // Permite enviar un archivo HTML
-});
-
 app.get("*", (req, res) => {
   res.send("Ruta restringida, hemos localizado la direccion de su computador"); // Permite enviar texto o codigo HTML
 });
+*/
 
-app.listen(3000, () => {
+app.listen(3002, () => {
   console.log("Servidor corriendo");
 });
-*/
+
+app.get('/home', (req,res) =>{
+  res.sendFile(path.join(__dirname, './views/home.html'));  // Permite enviar un archivo HTML
+});
+app.get('/shoppingCar', (req,res) =>{
+  res.sendFile(path.join(__dirname, './views/shoppingCar.html'));  // Permite enviar un archivo HTML
+});
