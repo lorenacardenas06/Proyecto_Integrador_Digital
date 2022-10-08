557b11c89d1b049bfe0e528176ad3d792e8745c3
Ailobenitez <ailem1312@gmail.com>
2022-10-08 14:50:02 -0300

    ingreso

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -2,6 +2,9 @@ const controladorUsuarios = {
   registrar: (req, res) => {
     res.render("login");
   }, //registar usuarios
+ singIN:(req, res)=> {
+    res.render("singIn");
+  }//inicio de sesion
 };
 
 module.exports = controladorUsuarios;
