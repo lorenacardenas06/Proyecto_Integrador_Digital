4da6c248ac9ca559cb3dd381519619f48a38b388
Ailobenitez <ailem1312@gmail.com>
2022-10-09 17:51:32 -0300

    DDD

--- src/routes/productosRouters.js
+++ src/routes/productosRouters.js
@@ -10,11 +10,8 @@ const productosController = require("../controllers/productosControllers");
 router.get("/", productosController.index);
 router.get("/detalleProducto", productosController.detalle);
 router.get("/carritoProducto", productosController.carro);
-<<<<<<< HEAD
-=======
 router.get("/crearProducto", productosController.crearProducto);
 
->>>>>>> 6c2c0c67f752c14260dae377a3ccb8a7041d8b82
 router.get("/productos/:id?", function(req,res) {
     req.params.id
 });
