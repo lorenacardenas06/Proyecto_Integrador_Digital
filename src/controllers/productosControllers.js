cd0ee1912245e1bc59c83e17d25985b9dc36e559
lorenacardenas06 <lorenacardenas06@gmail.com>
2022-10-10 18:14:30 -0500

    LorenaCambios

--- src/controllers/productosControllers.js
+++ src/controllers/productosControllers.js
@@ -7,12 +7,12 @@ const products = JSON.parse(fs.readFileSync(productosFilePath,'utf-8'));
 //------------OBJETO DEL CONTROLADOR------------------
 const controladorProductos = {
   index: (req, res) => {
-    res.render("home"); //mostrar pagina de inicio
+    res.render("home",{productos:products}); //mostrar pagina de inicio
   }, 
   
   detalle: (req, res) => {
 //   console.log(productos)
-    res.render("products/detalleProducto",{p:productosFilePath}); //mostrar detalle de un producto
+    res.render("products/detalleProducto"); //mostrar detalle de un producto
   }, 
   carro: (req, res) => {
     res.render("products/carritoProducto"); //mostrar carrito
