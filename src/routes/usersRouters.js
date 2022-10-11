66aa995a1ba8493900412a662129345d6ad2efe5
JulianaJt <juliana.trujillov@urosario.edu.co>
2022-10-10 21:03:24 -0500

    multer

--- src/routes/usersRouters.js
+++ src/routes/usersRouters.js
@@ -3,7 +3,7 @@ const controladorUsuarios = require("../controllers/usersControllers");
 
 const express = require("express");
 const router = express.Router();
-router.post("/registro",controladorUsuarios.registrarUsuario)
+router.post("/registro",controladorUsuarios.registrar)
 router.get("/ingreso", controladorUsuarios.ingreso);
 router.get("/perfil", controladorUsuarios.perfil);
 
