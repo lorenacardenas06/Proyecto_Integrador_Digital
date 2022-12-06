8a460fe425e8db53c75175459301d5fe8ea90df9
lorenacardenasch <lorenacardenas06@gmail.com>
2022-12-06 01:02:07 -0500

    LorenaCambios

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,27 +1,3 @@
-module.exports = {
-  "development": {
-    "username": "289889",
-    "password": Lore961206*,
-    "database": "bd_acmaquillaje",
-    "host": "mysql-lorenacardenas06.alwaysdata.net",
-    "dialect": "mysql",
-    "port": 3306
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
 require("dotenv").config();
 
 module.exports = {
@@ -32,5 +8,21 @@ module.exports = {
       "host": process.env.DB_HOST,
       "dialect": process.env.DB_DIALECT,
       "port":process.env.DB_PORT,
-}
+},
+    "test": {
+      "username": process.env.DB_USER,
+      "password": process.env.DB_PASSWORD,
+      "database": process.env.DB_DATABASE,
+      "host": process.env.DB_HOST,
+      "dialect": process.env.DB_DIALECT,
+      "port":process.env.DB_PORT,
+    },
+    "production": {
+      "username": process.env.DB_USER,
+      "password": process.env.DB_PASSWORD,
+      "database": process.env.DB_DATABASE,
+      "host": process.env.DB_HOST,
+      "dialect": process.env.DB_DIALECT,
+      "port":process.env.DB_PORT,
+    }
 }
--- src/database/models/producto.js
+++ src/database/models/producto.js
@@ -1,25 +1,20 @@
 function productoData(sequelize, Datatypes)
 {
-    let a = 'producto';
+    let a = 'Producto';
 
     let b = {
         id:{type:Datatypes.INTEGER, primaryKey: true, autoIncrement: true},
         nombre:{type:Datatypes.STRING(500)},
         precio:{type:Datatypes.INTEGER},
-        imagen:{type:Datatypes.STRING(500)},
+        imagen:{type:Datatypes.STRING(200)},
         descripcion:{type:Datatypes.STRING(1000)},
         fecha_eliminacion:{type:Datatypes.DATE},
     }
 
-    let c = {camelCase: false, timestamps: false};
+    let c = {camelCase: false, timestamps: false, tableName: 'Producto'};
     const productos= sequelize.define(a,b,c)
     return productos;
 }
 
-{
-    marca_id:{type:Datatypes.INTEGER},
-    categoria_id:{type:Datatypes.INTEGER},
-}
-    
 module.exports = productoData;
     
