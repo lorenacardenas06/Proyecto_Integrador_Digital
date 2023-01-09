545732e4dfd5d5a5b75f8ba8179692f1854cef76
Jorge <jorgecuadradovelasquez@gmail.com>
2023-01-09 16:31:51 -0500

    responsive creacion de productos y productos routes comentando lo de consultas api porque no funcionaba

--- src/routes/productosRouters.js
+++ src/routes/productosRouters.js
@@ -3,21 +3,23 @@
 const express = require("express");
 const router = express.Router();
 const productosController = require("../controllers/productosControllers");
-const multer  = require('multer'); //multer
-const path= require("path");
-const authMiddleware = require('../middlewares/authMiddleware.js')
-const {body} = require('express-validator');
+const multer = require("multer"); //multer
+const path = require("path");
+const authMiddleware = require("../middlewares/authMiddleware.js");
+const { body } = require("express-validator");
 
 //---------MULTER------------------------------------
 
 const multerDiskStorage = multer.diskStorage({
-    destination: function(req, file, cb) {       // request, archivo y callback que almacena archivo en destino
-     cb(null, path.join(__dirname,'../../public/img/products'));    // Ruta donde almacenamos el archivo
-    },
-    filename: function(req, file, cb) {          // request, archivo y callback que almacena archivo en destino
-     let imageName = Date.now() + path.extname(file.originalname);   // milisegundos y extensión de archivo original
-     cb(null, imageName);         
-    }
+  destination: function (req, file, cb) {
+    // request, archivo y callback que almacena archivo en destino
+    cb(null, path.join(__dirname, "../../public/img/products")); // Ruta donde almacenamos el archivo
+  },
+  filename: function (req, file, cb) {
+    // request, archivo y callback que almacena archivo en destino
+    let imageName = Date.now() + path.extname(file.originalname); // milisegundos y extensión de archivo original
+    cb(null, imageName);
+  },
 });
 const uploadFile = multer({ storage: multerDiskStorage });
 
@@ -32,14 +34,22 @@ router.get("/electricos", productosController.electrico);
 
 /***CREATE ALL PRODUCTS***/
 router.get("/crearProducto", productosController.crearProducto);
-router.post("/crearProducto", uploadFile.single('imagen'), productosController.store);
+router.post(
+  "/crearProducto",
+  uploadFile.single("imagen"),
+  productosController.store
+);
 
 /***GET ONE PRODUCT ***/
 router.get("/detalleProducto/:id", productosController.detalleProducto);
 
 /***EDIT ONE PRODUCT ***/
 router.get("/editarProducto/:id", productosController.editarProducto);
-router.put("/editarProducto/:id",uploadFile.single('imagen'), productosController.actualizarProducto);
+router.put(
+  "/editarProducto/:id",
+  uploadFile.single("imagen"),
+  productosController.actualizarProducto
+);
 
 /***DELETE ONE PRODUCT ***/
 router.delete("/:id", productosController.eliminarProducto);
@@ -48,8 +58,8 @@ router.delete("/:id", productosController.eliminarProducto);
 router.get("/carritoProducto", productosController.carritoProducto);
 
 /*** Consultas API***/
-router.get("/consultaProductos", productosController.consultaProducto);
-router.get("/consultaCategorias", productosController.consultaCategoria);
+// router.get("/consultaProductos", productosController.consultaProducto);
+// router.get("/consultaCategorias", productosController.consultaCategoria);
 
 //-----------EXPORTAR MODULO---------------------------
 module.exports = router;
